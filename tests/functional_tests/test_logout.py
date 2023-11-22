5328bf47e32a8dfdb71efad7b30e8041fdb92e96
doridoro <dorothea.reher@gmail.com>
2023-11-22 15:44:07 +0100

    modified test_logout, redirecion

--- tests/functional_tests/test_logout.py
+++ tests/functional_tests/test_logout.py
@@ -1,6 +1,7 @@
 """
 This file test_logout.py contains the functional tests for the route logout
 """
+from flask import url_for
 
 
 def test_logout(test_client):
@@ -12,4 +13,8 @@ def test_logout(test_client):
 
     response = test_client.get("/logout")
     assert response.status_code == 302
-    assert b"Welcome to the GUDLFT Registration Portal!"
+    # _external use the absolute url: http://localhost/ instead of /
+    assert response.location == url_for("index", _external=True)
+    # response.location holt the redirected url http://localhost/ (home page)
+    response = test_client.get(response.location)
+    assert b"Welcome to the GUDLFT Registration Portal!" in response.data
