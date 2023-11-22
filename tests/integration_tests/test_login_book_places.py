79dafc89387a8c928385c616ee6410df1cf969e1
doridoro <dorothea.reher@gmail.com>
2023-11-22 16:28:40 +0100

    modified test_login_book_places

--- tests/integration_tests/test_login_book_places.py
+++ tests/integration_tests/test_login_book_places.py
@@ -4,42 +4,72 @@ secretary and one purchase of places of a competition.
 - tests the login with pytest-mock
 - tests the purchase of places of one competition
 """
+from datetime import datetime
+
 from tests.mocks import (
     mock_load_clubs_valid,
     mock_load_competition_valid,
+    VALID_CLUB_EMAIL,
+    VALID_COMPETITION_NAME,
+    VALID_CLUB_NAME,
+    VALID_CLUB_POINTS,
+    INVALID_COMPETITION_NAME,
+    INVALID_CLUB_NAME,
+    VALID_COMPETITION_DATE,
 )
-from tests.functional_tests.test_book import (
-    test_book_valid_competition,
-    test_book_invalid_competition,
-)
-from tests.functional_tests.test_purchase_places import test_purchase_places_successful
-from tests.functional_tests.test_show_summary import test_show_summary_successful_login
 
 PLACES_INPUT_VALID = 5
 PLACES_INPUT_INVALID = 13
+PURCHASE_PLACES_VALID = 6
+PURCHASE_PLACES_INVALID = 15
 
 
 def test_login_and_book_places(mocker, test_client):
     mocker.patch("server.load_clubs", mock_load_clubs_valid)
     mocker.patch("server.load_competitions", mock_load_competition_valid)
 
-    test_show_summary_successful_login(test_client)
-    test_book_valid_competition(test_client)
-    test_purchase_places_successful(test_client)
+    test_client.post("/show-summary", data={"email": VALID_CLUB_EMAIL})
+    test_client.get(f"/book/{VALID_COMPETITION_NAME}/{VALID_CLUB_NAME}")
+    response = test_client.post(
+        "/purchase-places",
+        data={
+            "competition": VALID_COMPETITION_NAME,
+            "club": VALID_CLUB_NAME,
+            "places": PURCHASE_PLACES_VALID,
+        },
+    )
+    assert response.status_code == 200
+    result = int(VALID_CLUB_POINTS) - PURCHASE_PLACES_VALID
+    assert f"Points available: {result}" in str(response.data)
 
 
 def test_login_and_book_invalid_competition(mocker, test_client):
     mocker.patch("server.load_clubs", mock_load_clubs_valid)
     mocker.patch("server.load_competitions", mock_load_competition_valid)
 
-    test_show_summary_successful_login(test_client)
-    test_book_invalid_competition(test_client)
+    test_client.post("/show-summary", data={"email": VALID_CLUB_EMAIL})
+    today = datetime.now().replace(microsecond=0)
+    if VALID_COMPETITION_DATE < str(today):
+        response = test_client.get(
+            f"/book/{INVALID_COMPETITION_NAME}/{INVALID_CLUB_NAME}"
+        )
+        assert response.status_code == 200
+        assert b"Sorry, but this competition is already over!" in response.data
 
 
 def test_login_purchase_invalid_places_input(mocker, test_client):
     mocker.patch("server.load_clubs", mock_load_clubs_valid)
     mocker.patch("server.load_competitions", mock_load_competition_valid)
 
-    test_show_summary_successful_login(test_client)
-    test_book_valid_competition(test_client)
-    test_purchase_places_successful(test_client)
+    test_client.post("/show-summary", data={"email": VALID_CLUB_EMAIL})
+    test_client.get(f"/book/{VALID_COMPETITION_NAME}/{VALID_CLUB_NAME}")
+    response = test_client.post(
+        "/purchase-places",
+        data={
+            "competition": VALID_COMPETITION_NAME,
+            "club": VALID_CLUB_NAME,
+            "places": PURCHASE_PLACES_INVALID,
+        },
+    )
+    assert response.status_code == 200
+    assert b"You can not book more than 12 places!" in response.data
