45a3a600a459f7e1506cd841f6149ee8ecafa9d3
doridoro <dorothea.reher@gmail.com>
2023-11-22 15:39:56 +0100

    modified test_dashboard_login_book_logout

--- tests/integration_tests/test_dashboard_login_book_logout.py
+++ tests/integration_tests/test_dashboard_login_book_logout.py
@@ -4,44 +4,45 @@ dashboard, login of one secretary, purchase of places of a valid competition and
 - tests the login with pytest-mock
 - tests the purchase of places of one competition
 """
+from flask import url_for
+
 from tests.mocks import (
     mock_load_clubs_valid,
     mock_load_competition_valid,
+    VALID_CLUB_EMAIL,
+    VALID_COMPETITION_NAME,
+    VALID_CLUB_NAME,
+    VALID_CLUB_POINTS,
 )
-from tests.functional_tests.test_book import test_book_valid_competition
-from tests.functional_tests.test_dashboard import test_dashboard
-from tests.functional_tests.test_logout import test_logout
-from tests.functional_tests.test_purchase_places import test_purchase_places_successful
-from tests.functional_tests.test_show_summary import test_show_summary_successful_login
 
-PLACES_INPUT_VALID = 5
+PURCHASE_PLACES = 6
 
 
 def test_dashboard_login_book_logout(mocker, test_client):
     mocker.patch("server.load_clubs", mock_load_clubs_valid)
     mocker.patch("server.load_competitions", mock_load_competition_valid)
 
-    test_dashboard(test_client)
-    test_show_summary_successful_login(test_client)
-    test_book_valid_competition(test_client)
-    test_purchase_places_successful(test_client)
-    test_logout(test_client)
-
-    # test_client.post("/show-summary", data={"email": VALID_CLUB_EMAIL})
-    #
-    # test_client.get(f"/book/{VALID_COMPETITION_NAME}/{VALID_CLUB_NAME}")
-    #
-    # response = test_client.post(
-    #     "/purchase-places",
-    #     data={
-    #         "competition": VALID_COMPETITION_NAME,
-    #         "club": VALID_CLUB_NAME,
-    #         "places": 5,
-    #     },
-    #
-    #     competition["number_of_places"] = 15
-    # )
-    # assert response.status_code == 200
-    # assert b"Great-booking complete!"
-    # competition["number_of_places"] = 10
-    # # if the amount has changed   competition["number_of_places"] has changed
+    test_client.get("/dashboard")
+    test_client.get("/")
+    test_client.post("/show-summary", data={"email": VALID_CLUB_EMAIL})
+    test_client.get(f"/book/{VALID_COMPETITION_NAME}/{VALID_CLUB_NAME}")
+    response = test_client.post(
+        "/purchase-places",
+        data={
+            "competition": VALID_COMPETITION_NAME,
+            "club": VALID_CLUB_NAME,
+            "places": PURCHASE_PLACES,
+        },
+    )
+    assert response.status_code == 200
+    result = int(VALID_CLUB_POINTS) - PURCHASE_PLACES
+    assert f"Points available: {result}" in str(
+        response.data
+    )  # assert f"Points available: 7" in str(response.data)
+    response = test_client.get("/logout")
+    assert response.status_code == 302
+    # _external use the absolute url: http://localhost/ instead of /
+    assert response.location == url_for("index", _external=True)
+    # response.location holt the redirected url http://localhost/ (home page)
+    response = test_client.get(response.location)
+    assert b"Welcome to the GUDLFT Registration Portal!" in response.data
