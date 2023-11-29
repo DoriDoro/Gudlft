372d9d7f09fd1175abe68ef20c37277cb7c82990
doridoro <dorothea.reher@gmail.com>
2023-11-29 16:13:16 +0100

    possible improvements

--- server.py
+++ server.py
@@ -44,6 +44,25 @@ def show_summary():
     return render_template("welcome.html", club=club, competitions=competitions)
 
 
+"""
+improvements:
+    - use only dictionary instead of list and dictionary
+    - use a generator expression instead of a list comprehension to create 'club'.
+      The next function is used to get the first item from the generator, or 'None' if the 
+      generator is empty.
+    
+def show_summary():
+   email = request.form["email"]
+   club = next((club for club in clubs if club["email"] == email), None)
+
+   if club is None:
+       flash("Sorry, that email was not found.")
+       return render_template("index.html")
+
+   return render_template("welcome.html", club=club, competitions=competitions)
+"""
+
+
 @app.route("/book/<competition>/<club>")
 def book(competition, club):
     """renders the chosen and valid competition to book a place
@@ -64,6 +83,37 @@ def book(competition, club):
     return render_template(
         "booking.html", club=found_club, competition=found_competition
     )
+
+
+"""
+improvements:
+    - use only dictionary instead of list and dictionary
+    - use a generator expression instead of a list comprehension to create 'club'.
+      The next function is used to get the first item from the generator, or 'None' if the 
+      generator is empty.
+    - check if club and competition are existing before accessing them
+    
+def book(competition, club):
+
+   today = datetime.now().replace(microsecond=0)
+
+   found_club = next((c for c in clubs if c["name"] == club), None)
+   if found_club is None:
+       flash("Sorry, but this club is not found!")
+       
+   found_competition = next((c for c in competitions if c["name"] == competition), None)
+   if found_competition is None:
+       flash("Sorry, but this competition is not found!")
+
+   if found_competition["date"] < str(today):
+       flash("Sorry, but this competition is already over!")
+       return render_template(
+           "welcome.html", club=found_club, competitions=competitions
+       )
+   return render_template(
+       "booking.html", club=found_club, competition=found_competition
+   )
+"""
 
 
 @app.route("/purchase-places", methods=["POST"])
@@ -103,6 +153,97 @@ def purchase_places():
     club["points"] = int(club["points"]) - places_required
     flash("Great-booking complete!")
     return render_template("welcome.html", club=club, competitions=competitions)
+
+
+"""
+improvements:
+    - use only dictionary instead of list and dictionary
+    - use a generator expression instead of a list comprehension to create 'club'.
+      The next function is used to get the first item from the generator, or 'None' if the 
+      generator is empty.
+    - check if club and competition are existing before accessing them
+    
+def purchase_places():
+
+   competition_name = request.form["competition"]
+   club_name = request.form["club"]
+   places_required = int(request.form["places"])
+
+   competition = next((c for c in competitions if c["name"] == competition_name), None)
+   if competition is None:
+       flash("Competition not found!")
+   club = next((c for c in clubs if c["name"] == club_name), None)
+   if club is None:
+       flash("Club not found.")
+
+   if places_required > int(club["points"]):
+       flash("You try to book more places than you have points available!")
+   if places_required > int(competition["number_of_places"]):
+       flash("You try to book more than there are places available!")
+   if places_required > 12:
+       flash("You can not book more than 12 places!")
+   if places_required == 0:
+       flash("Please chose a number between 1 and 12!")
+
+   competition["number_of_places"] = (
+       int(competition["number_of_places"]) - places_required
+   )
+   club["points"] = int(club["points"]) - places_required
+   flash("Great-booking complete!")
+   return render_template("welcome.html", club=club, competitions=competitions)
+"""
+
+
+"""
+improvement: use Exceptions instead of flash messages
+    - have to create a new template 404.html
+
+from flask import abort
+
+class ClubNotFound(Exception):
+  pass
+
+class CompetitionNotFound(Exception):
+  pass
+
+@app.route("/purchase-places", methods=["POST"])
+def purchase_places():
+
+   competition_name = request.form["competition"]
+   club_name = request.form["club"]
+   places_required = int(request.form["places"])
+
+   competition = next((c for c in competitions if c["name"] == competition_name), None)
+   if competition is None:
+       abort(404, description="Competition not found.")
+   club = next((c for c in clubs if c["name"] == club_name), None)
+   if club is None:
+       abort(404, description="Club not found.")
+
+   if places_required > int(club["points"]):
+       abort(400, description="You try to book more places than you have points available!")
+   if places_required > int(competition["number_of_places"]):
+       abort(400, description="You try to book more than there are places available!")
+   if places_required > 12:
+       abort(400, description="You can not book more than 12 places!")
+   if places_required == 0:
+       abort(400, description="Please chose a number between 1 and 12!")
+
+   competition["number_of_places"] = (
+       int(competition["number_of_places"]) - places_required
+   )
+   club["points"] = int(club["points"]) - places_required
+   flash("Great-booking complete!")
+   return render_template("welcome.html", club=club, competitions=competitions)
+
+@app.errorhandler(404)
+def handle_404(e):
+  return render_template('404.html'), 404
+
+@app.errorhandler(400)
+def handle_400(e):
+  return render_template('400.html', error_message=e.description), 400
+"""
 
 
 @app.route("/dashboard")
