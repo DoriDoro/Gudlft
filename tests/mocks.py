7003b83b1a33f71c95eb43eea561bf1efcf8d082
doridoro <dorothea.reher@gmail.com>
2023-11-29 15:47:56 +0100

    update the competition date

--- tests/mocks.py
+++ tests/mocks.py
@@ -1,3 +1,7 @@
+from datetime import datetime, timedelta
+
+next_week_date = datetime.now() + timedelta(days=7)
+
 VALID_CLUB_EMAIL = "valid@email.com"
 INVALID_CLUB_EMAIL = "invalid@email.com"
 VALID_CLUB_NAME = "Valid Club Name"
@@ -8,7 +12,7 @@ INVALID_CLUB_POINTS = "0"
 VALID_COMPETITION_NAME = "Valid Competition Name"
 INVALID_COMPETITION_NAME = "Invalid Competition Name"
 VALID_COMPETITION_AVAILABLE_PLACES = "25"
-VALID_COMPETITION_DATE = "2024-03-27 10:00:00"
+VALID_COMPETITION_DATE = next_week_date.isoformat()
 
 
 def mock_load_clubs_valid():
