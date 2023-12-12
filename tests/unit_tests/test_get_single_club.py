ba88e73e0610b3018aabbbf099c89d955e9453dc
doridoro <dorothea.reher@gmail.com>
2023-12-12 14:51:12 +0100

    create test to check if the found competition is not found, None

--- tests/unit_tests/test_get_single_club.py
+++ tests/unit_tests/test_get_single_club.py
@@ -1,9 +1,11 @@
 """
 This file test_get_single_club.py contains the unit tests for the function get_single_club in
 server.py file.
-- tests the listing of the competition, if valid (in future)
-- tests the error message if a competition date is in the past
+- tests the function get_single_club:
+    - successful outcome
+    - failed outcome, None
 """
+
 import server
 from tests.mocks import mock_load_clubs_valid
 
@@ -17,3 +19,15 @@ def test_get_single_club_success(mock_clubs_valid):
 
     club_name = mock_load_clubs_valid()[0]["name"]
     assert server.get_single_club(club_name) == mock_load_clubs_valid()[0]
+
+
+def test_get_single_club_fail(mock_clubs_valid):
+    """
+    GIVEN a Flask app for testing
+    WHEN the function get_single_club is called
+    THEN checks if the found club is None
+    """
+
+    club_name = "wrong_club_name"
+    none_value = None
+    assert server.get_single_club(club_name) == none_value
